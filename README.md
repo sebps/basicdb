9845bd5f12c1a93e5f33c2c08d14f093b7f60a9b
sebps <sebpsdev@gmail.com>
2022-05-28 17:14:34 +0200

    data synchronization

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 pocdb
 ==========
 
-Lightweight key/value database.
+Lightweight eventually consistent key/value database.
 
 <!-- TOC -->
 
@@ -35,7 +35,16 @@ pocdb.stop()
 ### Parameters
 
 ### configuration 
-An object defining the path of the persisted data.
+An object defining pocdb configuration containing the following properties :
+
+#### path
+The path of the persisted data.
+
+### Eventual consistency
+Data synchronisation is supported accross several instances of pocdb referencing the same persisted data file. 
+A .pocdb log file is saved under the same location than the persisted data file. 
+Real-time updates to .pocdb log file are performed to achieve synchronization accross any number of pocdb instances referencing the data file. 
+Synchronization time is of 1 second, meaning eventual consistency for pocdb is reached after 1 second at most.
 
 ## License
 
--- core/index.js
+++ core/index.js
@@ -1,12 +1,16 @@
-const { readFileSync, writeFileSync, existsSync } = require("fs")
+const { readFileSync, writeFileSync, existsSync, appendFileSync } = require("fs")
+const syncFrequency = 30000
 
 var dataCopy
 var dataLocation
-var tick
+var logsLocation
+var syncDataTick
+var cleanLogsTick
+var lastSync
 
-const put = (key, value) => {
-    const keyPath = key.split(".")
-    let dataCursor = dataCopy
+const doPut = (key, value, data) => {
+    const keyPath = key.toString().split(".")
+    let dataCursor = data
     while(keyPath.length > 0) {
         const keySegment = keyPath.shift()
         if (keyPath.length == 0) {
@@ -18,9 +22,9 @@ const put = (key, value) => {
     }
 }
 
-const get = (key) => {
+const doGet = (key, data) => {
     const keyPath = key.split(".")
-    let dataCursor = dataCopy
+    let dataCursor = data
     while(keyPath.length > 0) {
         const keySegment = keyPath.shift()
         if (keyPath.length == 0) {
@@ -32,9 +36,9 @@ const get = (key) => {
     }
 }
 
-const destroy = (key) => {
+const doDestroy = (key, data) => {
     const keyPath = key.split(".")
-    let dataCursor = dataCopy
+    let dataCursor = data
     while(keyPath.length > 0) {
         const keySegment = keyPath.shift()
         if (keyPath.length == 0) {
@@ -45,19 +49,113 @@ const destroy = (key) => {
     }
 }
 
+const get = (key) => {
+    return doGet(key, dataCopy)
+}
+
+const put = (key, value) => {
+    doPut(key, value, dataCopy)
+    console.log("put")
+    console.log(dataCopy)
+    appendFileSync(logsLocation, `${Date.now()}:${key}:put\n`)
+    syncData(key)
+}
+
+const destroy = (key) => {
+    doDestroy(key, dataCopy)
+    appendFileSync(logsLocation, `${Date.now()}:${key}:destroy\n`)
+    syncData(key)
+}
+
+const syncData = (key) => {
+    console.log("syncing data ...")
+    
+    // sync local data copy with shared data
+    const startSync = Date.now()
+    
+    if (existsSync(dataLocation)) {
+        const lastDataRawCopy = readFileSync(dataLocation)
+        const lastDataCopy = JSON.parse(lastDataRawCopy)
+
+        if (existsSync(logsLocation)) {
+            const logRawCopy = readFileSync(logsLocation)
+            logs = logRawCopy.toString().split('\n')
+            // loop over logs to detect most recent versions of data records
+            for (const log of logs) {
+                const [ logTimestamp, logKey, logOp ] = log.split(':')
+                if ( logTimestamp > lastSync && logKey != key) {
+                    switch(logOp) {
+                        case 'put':
+                            const lastValue = doGet(logKey, lastDataCopy)
+                            doPut(logKey, lastValue, dataCopy)
+                        break;
+                        case 'destroy':
+                            doDestroy(logKey, dataCopy)
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    lastSync = startSync
+    
+    console.log("data copy at the end of syncing : ")
+    console.log(dataCopy)
+    console.log("data location : ")
+    console.log(dataLocation)
+
+    try {
+        writeFileSync(dataLocation, JSON.stringify(dataCopy))
+    } catch(err) {
+        console.log(err)
+    }
+}
+
+const cleanLogs = () => {
+    console.log("cleaning logs ...")
+
+    const now = Date.now()
+
+    // clean logs older than 2 * syncFrequency ( so that everyone syncing with db at syncFrequency should already be synchronized with those logs )    
+    if (existsSync(logsLocation)) {
+        const logRawCopy = readFileSync(logsLocation)
+        const logs = logRawCopy.toString().split('\n')
+        const cleanedLogs = []
+
+        // loop over logs to detect most recent versions of data records
+        for (const log of logs) {
+            const [ timestamp ] = log.split(':')
+            if ( timestamp > now - 2 * syncFrequency ) {
+                cleanedLogs.push(log)
+            }
+        }
+
+        writeFileSync(logsLocation, cleanedLogs.join('\n'))
+    }
+}
+
 const start = ({ path }) => {
     if (!path) throw "missing path to data file in config"
 
     dataLocation = path 
+    logsLocation = path + ".pocdb" 
 
     if (existsSync(dataLocation)) {
-        dataRawCopy = readFileSync(dataLocation)
+        const dataRawCopy = readFileSync(dataLocation)
         dataCopy = JSON.parse(dataRawCopy)
+        lastSync = Date.now()
     } else {
         dataCopy = {}
     }
 
-    tick = setInterval(() => {}, 1000)
+    syncDataTick = setInterval(() => {
+        syncData()
+    }, syncFrequency)
+
+    cleanLogsTick = setInterval(() => {
+        cleanLogs()
+    }, syncFrequency)
 
     process.on('SIGINT', () => {
         console.log(`SIGINT : persisting db at ${dataLocation} ...`)
@@ -71,8 +169,10 @@ const start = ({ path }) => {
 }
 
 const stop = () => {
-    if(tick) clearInterval(tick)
-    if( dataLocation && dataCopy) writeFileSync(dataLocation, JSON.stringify(dataCopy))
+    if(syncDataTick) clearInterval(syncDataTick)
+    if(cleanLogsTick) clearInterval(cleanLogsTick)
+
+    syncData()
 }
 
 module.exports = { start, stop, get, put, destroy }
